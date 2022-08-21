763c84e232bab90c5e7682e3b3264518bbd0814c
chosunone <chosunone@protonmail.com>
2022-08-22 02:43:34 +0900

    refactor(right_panel_renderer): add structure to right panel renderer

--- src/ui/right_panel_renderer.rs
+++ src/ui/right_panel_renderer.rs
@@ -6,7 +6,7 @@ use crate::ui::selection::{
 };
 use crate::{MapError, MapLoader, MapMode};
 use actix::Addr;
-use egui::{Context, Pos2, SidePanel, TopBottomPanel};
+use egui::{Context, Pos2, SidePanel, TopBottomPanel, Ui};
 use indicatif::InMemoryTerm;
 use log::{debug, trace};
 use world_gen::components::prelude::{Definition, StrategicRegion};
@@ -17,6 +17,13 @@ use world_gen::map::{
     GetStateIdFromPoint, GetStrategicRegionFromId, GetStrategicRegionIdFromPoint, Map,
 };
 use world_gen::MapDisplayMode;
+
+struct SelectedRegions {
+    selected_strategic_region: Option<StrategicRegion>,
+    selected_state: Option<State>,
+    selected_province: Option<Definition>,
+    selected_point: Option<Pos2>,
+}
 
 pub struct RightPanelRenderer {
     map_mode: Addr<MapMode>,
@@ -41,61 +48,14 @@ impl RightPanelRenderer {
         }
     }
 
-    #[allow(clippy::too_many_lines)]
     pub async fn render_right_panel(&self, ctx: &Context) -> Result<(), MapError> {
         let map_mode: MapDisplayMode = self.map_mode.send(GetMapMode).await?;
         let map_addr: Option<Addr<Map>> = self.map_loader.send(GetMap).await?;
-        let selected_point: Option<Pos2> = self.selection.send(GetSelectedPoint).await?;
-        let selected_province: Option<Definition> =
-            self.selection.send(GetSelectedProvince).await?;
-        let selected_state: Option<State> = self.selection.send(GetSelectedState).await?;
-        let selected_strategic_region: Option<StrategicRegion> =
-            self.selection.send(GetSelectedStrategicRegion).await?;
-        if let (Some(map), Some(point)) = (map_addr.clone(), selected_point) {
-            match map_mode {
-                MapDisplayMode::HeightMap | MapDisplayMode::Terrain | MapDisplayMode::Rivers => {}
-                MapDisplayMode::Provinces => {
-                    if selected_province.is_none() {
-                        // TODO: Perhaps reconsider where this logic should live
-                        if let Some(province_id) =
-                            map.send(GetProvinceIdFromPoint::new(point)).await?
-                        {
-                            if let Some(def) = map
-                                .send(GetProvinceDefinitionFromId::new(province_id))
-                                .await?
-                            {
-                                self.selection.send(SetSelectedProvince::new(def)).await?;
-                            }
-                        }
-                    }
-                }
-                MapDisplayMode::StrategicRegions => {
-                    if selected_strategic_region.is_none() {
-                        if let Some(sr_id) =
-                            map.send(GetStrategicRegionIdFromPoint::new(point)).await?
-                        {
-                            if let Some(sr) = map.send(GetStrategicRegionFromId::new(sr_id)).await?
-                            {
-                                self.selection
-                                    .send(SetSelectedStrategicRegion::new(sr))
-                                    .await?;
-                            }
-                        }
-                    }
-                }
-                MapDisplayMode::States => {
-                    if selected_state.is_none() {
-                        if let Some(s_id) = map.send(GetStateIdFromPoint::new(point)).await? {
-                            if let Some(s) = map.send(GetStateFromId::new(s_id)).await? {
-                                self.selection.send(SetSelectedState::new(s)).await?;
-                            }
-                        }
-                    }
-                }
-                m => {}
-            }
-        }
-        let continent_index = selected_province
+        let selected_regions = self.get_selected_regions().await?;
+        self.update_selected_regions(map_mode, &map_addr, &selected_regions)
+            .await?;
+        let continent_index = selected_regions
+            .selected_province
             .clone()
             .map(|d| d.continent)
             .filter(|c| c.0 > 0);
@@ -109,81 +69,193 @@ impl RightPanelRenderer {
             .resizable(false)
             .min_width(200.0)
             .show(ctx, |ui| {
-                TopBottomPanel::top("info_panel")
-                    .min_height(200.0)
-                    .show_inside(ui, |ui| match map_mode {
-                        MapDisplayMode::Provinces => {
-                            ui.label("Province Information");
-                            if let (Some(_), Some(_), Some(definition)) =
-                                (map_addr, selected_point, selected_province)
-                            {
-                                ui.label(format!("Id: {:?}", definition.id.0));
-                                ui.label(format!(
-                                    "Color: ({:?}, {:?}, {:?})",
-                                    definition.r.0, definition.g.0, definition.b.0
-                                ));
-                                ui.label(format!("Type: {:?}", definition.province_type));
-                                ui.label(format!("Coastal: {:?}", definition.coastal.0));
-                                ui.label(format!("Terrain: {:?}", definition.terrain.0));
-                                continent.map(|c| ui.label(format!("Continent: {:?}", c.0)));
-                            }
-                        }
-                        MapDisplayMode::States => {
-                            ui.label("State Information");
-                            if let (Some(_), Some(_), Some(state)) =
-                                (map_addr, selected_point, selected_state)
-                            {
-                                ui.label(format!("Id: {:?}", state.id.0));
-                                ui.label(format!("Name: {:?}", state.name.0));
-                                ui.label(format!(
-                                    "Manpower: {:?}",
-                                    state.manpower[state.manpower.len() - 1].0
-                                ));
-                                if let Some(supplies) = state.local_supplies {
-                                    ui.label(format!("Local Supplies: {:?}", supplies.0));
-                                }
-                                if let Some(max_level_factor) = state.buildings_max_level_factor {
-                                    ui.label(format!(
-                                        "Buildings Max Level Factor: {:?}",
-                                        max_level_factor.0
-                                    ));
-                                }
-                                if let Some(impassable) = state.impassable {
-                                    ui.label(format!("Impassable: {:?}", impassable));
-                                }
-                                ui.label(format!(
-                                    "Category: {:?}",
-                                    state.state_category[state.state_category.len() - 1].0
-                                ));
-                            }
-                        }
-                        MapDisplayMode::StrategicRegions => {
-                            ui.label("Strategic Region Information");
-                            if let (Some(_), Some(_), Some(sr)) =
-                                (map_addr, selected_point, selected_strategic_region)
-                            {
-                                ui.label(format!("Id: {:?}", sr.id.0));
-                                ui.label(format!("Name: {:?}", sr.name.0));
-                            }
-                        }
-                        MapDisplayMode::HeightMap
-                        | MapDisplayMode::Terrain
-                        | MapDisplayMode::Rivers => {}
-                        m => {
-                            ui.label(format!("Unknown map mode: {m}"));
-                        }
-                    });
-                TopBottomPanel::bottom("log_panel")
-                    .max_height(200.0)
-                    .show_inside(ui, |ui| {
-                        ui.label("Log Panel");
-                        ui.set_style(egui::Style {
-                            wrap: Some(false),
-                            ..Default::default()
-                        });
-                        ui.label(self.terminal.contents());
-                    });
+                render_info_panel(map_mode, &map_addr, &selected_regions, continent, ui);
+                self.render_log_panel(ui);
             });
         Ok(())
+    }
+
+    async fn update_selected_regions(
+        &self,
+        map_mode: MapDisplayMode,
+        map_addr: &Option<Addr<Map>>,
+        selected_regions: &SelectedRegions,
+    ) -> Result<(), MapError> {
+        if let (Some(map), Some(point)) = (map_addr.clone(), selected_regions.selected_point) {
+            match map_mode {
+                MapDisplayMode::HeightMap | MapDisplayMode::Terrain | MapDisplayMode::Rivers => {}
+                MapDisplayMode::Provinces => {
+                    if selected_regions.selected_province.is_none() {
+                        if let Some(province_id) =
+                            map.send(GetProvinceIdFromPoint::new(point)).await?
+                        {
+                            if let Some(def) = map
+                                .send(GetProvinceDefinitionFromId::new(province_id))
+                                .await?
+                            {
+                                self.selection.send(SetSelectedProvince::new(def)).await?;
+                            }
+                        }
+                    }
+                }
+                MapDisplayMode::StrategicRegions => {
+                    if selected_regions.selected_strategic_region.is_none() {
+                        if let Some(sr_id) =
+                            map.send(GetStrategicRegionIdFromPoint::new(point)).await?
+                        {
+                            if let Some(sr) = map.send(GetStrategicRegionFromId::new(sr_id)).await?
+                            {
+                                self.selection
+                                    .send(SetSelectedStrategicRegion::new(sr))
+                                    .await?;
+                            }
+                        }
+                    }
+                }
+                MapDisplayMode::States => {
+                    if selected_regions.selected_state.is_none() {
+                        if let Some(s_id) = map.send(GetStateIdFromPoint::new(point)).await? {
+                            if let Some(s) = map.send(GetStateFromId::new(s_id)).await? {
+                                self.selection.send(SetSelectedState::new(s)).await?;
+                            }
+                        }
+                    }
+                }
+                m => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn get_selected_regions(&self) -> Result<SelectedRegions, MapError> {
+        let selected_point: Option<Pos2> = self.selection.send(GetSelectedPoint).await?;
+        let selected_province: Option<Definition> =
+            self.selection.send(GetSelectedProvince).await?;
+        let selected_state: Option<State> = self.selection.send(GetSelectedState).await?;
+        let selected_strategic_region: Option<StrategicRegion> =
+            self.selection.send(GetSelectedStrategicRegion).await?;
+        let selected_regions = SelectedRegions {
+            selected_strategic_region,
+            selected_state,
+            selected_province,
+            selected_point,
+        };
+        Ok(selected_regions)
+    }
+
+    fn render_log_panel(&self, ui: &mut Ui) {
+        TopBottomPanel::bottom("log_panel")
+            .max_height(200.0)
+            .show_inside(ui, |ui| {
+                ui.label("Log Panel");
+                ui.set_style(egui::Style {
+                    wrap: Some(false),
+                    ..Default::default()
+                });
+                ui.label(self.terminal.contents());
+            });
+    }
+}
+
+fn render_info_panel(
+    map_mode: MapDisplayMode,
+    map_addr: &Option<Addr<Map>>,
+    selected_regions: &SelectedRegions,
+    continent: Option<Continent>,
+    ui: &mut Ui,
+) {
+    TopBottomPanel::top("info_panel")
+        .min_height(200.0)
+        .show_inside(ui, |ui| match map_mode {
+            MapDisplayMode::Provinces => {
+                render_province_info(map_addr, selected_regions, continent, ui);
+            }
+            MapDisplayMode::States => {
+                render_state_info(map_addr, selected_regions, ui);
+            }
+            MapDisplayMode::StrategicRegions => {
+                render_strategic_region_info(map_addr, selected_regions, ui);
+            }
+            MapDisplayMode::HeightMap | MapDisplayMode::Terrain | MapDisplayMode::Rivers => {}
+            m => {
+                ui.label(format!("Unknown map mode: {m}"));
+            }
+        });
+}
+
+fn render_strategic_region_info(
+    map_addr: &Option<Addr<Map>>,
+    selected_regions: &SelectedRegions,
+    ui: &mut Ui,
+) {
+    ui.label("Strategic Region Information");
+    if let (Some(_), Some(_), Some(sr)) = (
+        map_addr,
+        selected_regions.selected_point,
+        &selected_regions.selected_strategic_region,
+    ) {
+        ui.label(format!("Id: {:?}", sr.id.0));
+        ui.label(format!("Name: {:?}", sr.name.0));
+    }
+}
+
+fn render_state_info(
+    map_addr: &Option<Addr<Map>>,
+    selected_regions: &SelectedRegions,
+    ui: &mut Ui,
+) {
+    ui.label("State Information");
+    if let (Some(_), Some(_), Some(state)) = (
+        map_addr,
+        selected_regions.selected_point,
+        &selected_regions.selected_state,
+    ) {
+        ui.label(format!("Id: {:?}", state.id.0));
+        ui.label(format!("Name: {:?}", state.name.0));
+        ui.label(format!(
+            "Manpower: {:?}",
+            state.manpower[state.manpower.len() - 1].0
+        ));
+        if let Some(supplies) = state.local_supplies {
+            ui.label(format!("Local Supplies: {:?}", supplies.0));
+        }
+        if let Some(max_level_factor) = state.buildings_max_level_factor {
+            ui.label(format!(
+                "Buildings Max Level Factor: {:?}",
+                max_level_factor.0
+            ));
+        }
+        if let Some(impassable) = state.impassable {
+            ui.label(format!("Impassable: {:?}", impassable));
+        }
+        ui.label(format!(
+            "Category: {:?}",
+            state.state_category[state.state_category.len() - 1].0
+        ));
+    }
+}
+
+fn render_province_info(
+    map_addr: &Option<Addr<Map>>,
+    selected_regions: &SelectedRegions,
+    continent: Option<Continent>,
+    ui: &mut Ui,
+) {
+    ui.label("Province Information");
+    if let (Some(_), Some(_), Some(definition)) = (
+        map_addr,
+        selected_regions.selected_point,
+        &selected_regions.selected_province,
+    ) {
+        ui.label(format!("Id: {:?}", definition.id.0));
+        ui.label(format!(
+            "Color: ({:?}, {:?}, {:?})",
+            definition.r.0, definition.g.0, definition.b.0
+        ));
+        ui.label(format!("Type: {:?}", definition.province_type));
+        ui.label(format!("Coastal: {:?}", definition.coastal.0));
+        ui.label(format!("Terrain: {:?}", definition.terrain.0));
+        continent.map(|c| ui.label(format!("Continent: {:?}", c.0)));
     }
 }
